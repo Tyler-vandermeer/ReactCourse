a457c8e958b3bb39890df34d19c91651caee989a
Tyler Vandermeer <tyvdm9@gmail.com>
2022-05-28 15:06:42 -0400

    add faker

--- components/src/index.js
+++ components/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { faker } from '@faker-js/faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" />
+                    <img alt="avatar" src={faker.image.avatar()} />
                 </a>
                 <div className="content">
-                    <a href="/" className="author">Sam</a>
+                    <a href="/" className="author">{faker.name.firstName()}</a>
                     <div className="metadata">
                         <span className="date">Today at 6:00PM</span>
                     </div>
