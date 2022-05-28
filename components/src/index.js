2525b6139d6ad70a917e63eca8225aaa0f1c12f6
Tyler Vandermeer <tyvdm9@gmail.com>
2022-05-28 16:06:51 -0400

    Add props to comment component

--- components/src/index.js
+++ components/src/index.js
@@ -7,9 +7,9 @@ import ComponentDetail from './components/CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ComponentDetail />
-            <ComponentDetail />
-            <ComponentDetail />
+            <ComponentDetail author="Sam" timeAgo="Today at 4:45PM" commentText="Nice Post 1" avatarSrc={faker.image.avatar()} />
+            <ComponentDetail author="Alex" timeAgo="Today at 2:00AM" commentText="Nice Post 2" avatarSrc={faker.image.avatar()} />
+            <ComponentDetail author="Jane" timeAgo="Yesterday at 5:00PM" commentText="Nice Post 3" avatarSrc={faker.image.avatar()} />
         </div>
     );
 }
