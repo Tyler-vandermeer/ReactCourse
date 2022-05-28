952fa4a3f913e9936152c515dfc6a3b1e3a35941
Tyler Vandermeer <tyvdm9@gmail.com>
2022-05-28 16:20:47 -0400

    Add approval card component

--- components/src/index.js
+++ components/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { faker } from '@faker-js/faker';
 
 import ComponentDetail from './components/CommentDetail';
+import ApprovalCard from './components/ApprovalCard';
+import CommentDetail from './components/CommentDetail';
 
 const App = () => {
     return (
@@ -10,6 +12,9 @@ const App = () => {
             <ComponentDetail author="Sam" timeAgo="Today at 4:45PM" commentText="Nice Post 1" avatarSrc={faker.image.avatar()} />
             <ComponentDetail author="Alex" timeAgo="Today at 2:00AM" commentText="Nice Post 2" avatarSrc={faker.image.avatar()} />
             <ComponentDetail author="Jane" timeAgo="Yesterday at 5:00PM" commentText="Nice Post 3" avatarSrc={faker.image.avatar()} />
+            <ApprovalCard>
+                <CommentDetail author="Bill" timeAgo="Yesterday at 3:00PM" commentText="Nice Post 4" avatarSrc={faker.image.avatar()} />
+            </ApprovalCard>
         </div>
     );
 }
