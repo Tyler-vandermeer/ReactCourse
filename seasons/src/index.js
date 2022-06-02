716b0e216de4797bdb274057a827a5ecef7003d3
Tyler Vandermeer <tyvdm9@gmail.com>
2022-06-01 21:19:27 -0400

    Finished state videos

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,15 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMessage: "" };
+
         window.navigator.geolocation.getCurrentPosition(
-            (pos) => console.log(pos),
-            (err) => console.log(err)
+            (pos) => this.setState({ lat: pos.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
-        
-        return (
-            <div>Latitude: </div>
-        );
+    }
+
+    render() {
+        if (this.state.errorMessage) {
+            return <h2>Error: {this.state.errorMessage}</h2>
+        } else if (this.state.lat) {
+            return <h2>Latitude: {this.state.lat}</h2>
+        }
+        return <h2>Loading...</h2>
     }
 }
 
