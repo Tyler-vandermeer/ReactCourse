f1a1a7b3f7f2644adff03aa4e44a0cfa81d68c11
Tyler Vandermeer <tyvdm9@gmail.com>
2022-06-01 20:35:49 -0400

    initialize seasons project

---
+++ seasons/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (pos) => console.log(pos),
+            (err) => console.log(err)
+        );
+        
+        return (
+            <div>Latitude: </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
