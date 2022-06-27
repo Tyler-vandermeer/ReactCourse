51717e0f08594833feecc60b30b12353ae5d9876
Tyler Vandermeer <tyvdm9@gmail.com>
2022-06-27 09:35:17 -0400

    implement on change for search bar

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    state = { text: '' };
+    state = { term: '' };
 
-    onInputChange(event) {
-        this.setState({ text: event.target.value })
+    onFormSubmit(event) {
+        event.preventDefault();
     }
 
     render() {
         return (
             <div className='ui segment'>
-                <form className='ui form'>
+                <form className='ui form' onSubmit={this.onFormSubmit}>
                     <div className='field'>
                         <label for='search'>Image Search</label>
-                        <input id='search' type='text' onChange={ this.onInputChange } />
+                        <input id='search' type='text' value={this.state.term} onChange={ e => this.setState({ term: e.target.value}) } />
                     </div>
                 </form>
             </div>
