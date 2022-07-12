a5af8d440c7527c5bd3f1cb3d73bbf1ed117d52c
Tyler Vandermeer <tyvdm9@gmail.com>
2022-07-11 21:54:26 -0400

    Finish image list project

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,12 +1,22 @@
+import './ImageList.css'
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
-    const images = props.images.map(({description, id, urls}) => {
-        return <img alt={description} key={id} src={urls.regular} />
+    // const images = props.images.map(({description, id, urls}) => {
+    //     return <li key={id}><img alt={description} src={urls.regular} /></li>
+    // });
+
+    const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image} />
     });
 
     return (
-        <div>
+        // <ul className='image-list'>
+        //     {images}
+        // </ul>
+
+        <div className='image-list'>
             {images}
         </div>
     )
